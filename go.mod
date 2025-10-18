5b2c05f6c066c196e161910a12ad1a07c798f576
daria_bilan <daria.bilan02@gmail.com>
2025-10-18 16:21:26 +0300

    Add .gitignore file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module MyProject
+
+go 1.25.2
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	score := 15
+
+	if score > 10 {
+		fmt.Println("You are a cool person!")
+	} else {
+		fmt.Println("You should improve your skills")
+	}
+
+}
